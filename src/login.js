3a4f413a0d4f6ca5dd223e05266d882a838b68df
Rasika Hatwar <rasikahatwar13@gmail.com>
2020-12-20 01:43:26 +0530

    local storage code updated

--- src/login.js
+++ src/login.js
@@ -1,22 +1,26 @@
-function store(){
+const userList = JSON.parse(localStorage.getItem('userList'));
+const userName = localStorage.getItem('userName');
+const itemList = JSON.parse(localStorage.getItem('groceyItem'));
+console.log(userList,userName);
 
-    var name = document.getElementById('name');
+if(userName === null || userList=== null || itemList===null){
+    localStorage.setItem('userName','');
+    localStorage.setItem('userList',JSON.stringify([]) );
+    localStorage.setItem('groceyItem',JSON.stringify({}));
+}
+if(userName !== '' && userName != null){
+    location.replace("/views/home.html");
+}
+function store(){
     
+    var name = document.getElementById('name');
     if(name.value.length == 0){
         alert('Please enter user name');
-    }else{
-        localStorage.setItem('name', name.value);
-        console.log("data entry")
+    }
+    else{
+        localStorage.setItem('userName', name.value);
+        location.replace("/views/home.html");
     }
 }
 
-//checking
-function check(){
-    var storedName = localStorage.getItem('name');
-    var userName = document.getElementById('userName');
-    if(userName.value == storedName){
-        alert('Welcome');
-    }else{
-        alert('Error on login');
-    }
-}
+
